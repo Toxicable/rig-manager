d7f9aff8016ae52f20f783e4f9c7fcaee53b47d2
Toxicable <fabianwiles@live.com>
2017-05-13 09:48:08 +1200

    Added material theming

--- web-client/src/app/app.module.ts
+++ web-client/src/app/app.module.ts
@@ -15,7 +15,9 @@ import { AngularFireDatabaseModule } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { AppComponent } from './app.component';
 import { LoginComponent } from "app/auth/login.component";
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MdButtonModule, MdCheckboxModule, MdToolbarModule, MdInputModule, MdSelectModule} from '@angular/material';
+import { FlexLayoutModule } from '@angular/flex-layout';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/catch';
 import 'rxjs/add/operator/map';
@@ -24,6 +26,8 @@ import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/observable/timer';
 import 'rxjs/add/observable/forkJoin';
 import 'rxjs/add/observable/empty';
+
+import 'hammerjs';
 
 @NgModule({
   declarations: [
@@ -34,6 +38,13 @@ import 'rxjs/add/observable/empty';
   ],
   imports: [
     appRouting,
+    MdButtonModule,
+    MdCheckboxModule,
+    MdToolbarModule,
+    MdInputModule,
+    MdSelectModule,
+    FlexLayoutModule,
+    BrowserAnimationsModule,
     ReactiveFormsModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireDatabaseModule,
