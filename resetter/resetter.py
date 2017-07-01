75dfd4bac1ba3c479619f9b2a7a08ba6f317f8cf
Fabian Wiles <fabianwiles@live.com>
2017-07-02 01:11:52 +1200

    Added resetter back in and working

--- resetter/resetter.py
+++ resetter/resetter.py
@@ -1,16 +1,24 @@
-# pylint: disable-all
 import sys
 import pyrebase
 import urllib2
 import datetime
+import threading
 
-from threading import Timer
 from time import sleep
 from pyA20.gpio import gpio
 from pyA20.gpio import port
 
 def log(msg):
-  print(msg)
+  st = datetime.datetime.fromtimestamp(ts).strftime('%Y-%m-%d %H:%M:%S')
+  print( '[' + st + '] ' + str(msg))
+
+def set_interval(func, sec):
+  def func_wrapper():
+      set_interval(func, sec)
+      func()
+  t = threading.Timer(sec, func_wrapper)
+  t.start()
+  return t
 
 log('settting up GPIO')
 gpio.init()
@@ -37,16 +45,17 @@ def reset(port):
   sleep(10)
   pressButton(port)
 
-argsConfig = {}
+args = {}
 
 for i in range((len(sys.argv) - 1)/2):
-    argsConfig[sys.argv[i * 2 + 1]] = sys.argv[i * 2 + 2]
+    args[sys.argv[i * 2 + 1]] = sys.argv[i * 2 + 2]
 
-if not '-email' in argsConfig or not '-password' in argsConfig:
+if not '-email' in args or not '-password' in args:
   log('your must provide an email and password , for example: python resetter.py -email myemail@email.com -password 123456')
+  sys.exit()
 
-email = argsConfig['-email']
-password = argsConfig['-password']
+email = args['-email']
+password = args['-password']
 
 log('email: ' + email)
 
@@ -62,63 +71,60 @@ firebaseConfig = {
 log('setting up DB')
 firebase = pyrebase.initialize_app(firebaseConfig)
 auth = firebase.auth()
-global user
 user = auth.sign_in_with_email_and_password(email, password)
+userId = user['localId']
 db = firebase.database()
 
 def rigsChanged(message):
   try:
-    log('noticed a change in the DB, checking to see if we should restart something')
-    for rig in db.child(user['localId'] + '/rigs').get(user['idToken']).each():
+    log('DB change fired')
+    for rig in db.child(userId + '/rigs').get(user['idToken']).each():
       value = rig.val()
       key = rig.key()
       if not value:
         continue
       if(value['shouldRestart']):
         reset(value['port'])
-        db.child(user['localId'] + '/rigs/' + str(key) + '/shouldRestart').set(False, user['idToken'])
-    log('check done')
+        db.child(userId + '/rigs/' + str(key) + '/shouldRestart').set(False, user['idToken'])
   except Exception as ex:
     log('an error occured :/')
     log(ex.message)
-    pass
-
-rigStream = db.child(user['localId'] + '/rigs').stream(rigsChanged, user['idToken'])
+  finally:
+    log('check done')
 
 def pingRigs():
   try:
     log('pinging rigs')
-    db.child(user['localId'] + '/lastPingCheck').set(datetime.datetime.now().isoformat(), user['idToken'])
-    for rig in db.child(user['localId'] + '/rigs').get(user['idToken']).each():
+    db.child(userId + '/lastPingCheck').set(datetime.datetime.now().isoformat(), user['idToken'])
+    for rig in db.child(userId + '/rigs').get(user['idToken']).each():
       value = rig.val()
       if value and 'ipAddress' in value:
         try:
           body = urllib2.urlopen('http://' + value['ipAddress'] + ':3333').read()
+          if not body:
+            reset(value['port'])
         except Exception as ex:
           log('failed to ping IP:' + value['ipAddress'])
           log(ex.message)
           reset(value['port'])
           pass
-        if not body:
-          reset(value['port'])
+
   except Exception as ex:
     log('an error occured :/')
     log(ex.message)
-    pass
+  finally:
+    log('done pinging rigs')
 
-
-#aliveTimer = Timer(5 * 60, pingRigs)
-#aliveTimer.start()
-#pingRigs()
-
-user = {}
 def refresh_token():
+  global user
+  global rigStream
   log('refreshing auth token')
-  refreshToken = user['refreshToken']
-  user = auth.refresh(refreshToken)
-  rigStream = db.child(user['localId'] + '/rigs').stream(stream_handler, user['idToken'])
+  user = auth.refresh(user['refreshToken'])
+  rigStream = db.child(userId + '/rigs').stream(rigsChanged, user['idToken'])
 
-refreshTimer = Timer(20 * 60, refresh_token)
-refreshTimer.start()
+rigStream = db.child(userId + '/rigs').stream(rigsChanged, user['idToken'])
+pingInterval = set_interval(pingRigs, 5 * 60)
+refreshInterval = set_interval(refresh_token, 20 * 60)
 
 log('ready to go!')
+input()
