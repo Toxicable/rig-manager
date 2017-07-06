1f82ae91e041afb4cc9c6c0003c84bca761ca201
Fabian Wiles <fabianwiles@live.com>
2017-07-06 14:34:08 +1200

    update mount script

--- burst/mount.sh
+++ burst/mount.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 cd /dev/disk/by-label
+sudo mkdir ~/drives
 for label in *
 do
     partition=$(basename $(readlink $label))
-    sudo mkdir ~/mining/drives/$label
-    sudo mount /dev/$partition ~/mining/drives/$label
+    sudo mkdir ~/drives/$label
+    sudo mount /dev/$partition ~/drives/$label
 done
 exit
 
