25d375e1b6901bbbb1005760cd85795cb53cc3ca
Fabian Wiles <fabianwiles@live.com>
2017-08-01 21:26:30 +1200

    update conf

---
+++ burst/plot.sh
@@ -0,0 +1,25 @@
+sudo ./gpuPlotGenerator generate buffer
+~/drives/disk-2-9/16956931492307842315_209000000000_11059200_8192
+~/drives/disk-2-10/16956931492307842315_210000000000_11059200_8192
+~/drives/disk-2-11/16956931492307842315_211000000000_11059200_8192
+~/drives/disk-2-12/16956931492307842315_212000000000_11059200_8192
+
+2.7 11059200
+id 16956931492307842315
+
+2 01 0 0000 0000
+
+1 10 0 0000 0000_10240000_2048
+ /home/server1/drives/disk-2-10
+"/home/server1/drives/disk-2-1",
+"/home/server1/drives/disk-2-2",
+"/home/server1/drives/disk-2-3",
+"/home/server1/drives/disk-2-4",
+"/home/server1/drives/disk-2-5",
+"/home/server1/drives/disk-2-6",
+"/home/server1/drives/disk-2-7",
+"/home/server1/drives/disk-2-8",
+"/home/server1/drives/disk-2-9",
+"/home/server1/drives/disk-2-10",
+"/home/server1/drives/disk-2-11",
+"/home/server1/drives/disk-2-12"
